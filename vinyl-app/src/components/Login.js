c8abf88bdc9c485be372c9c6f6b65b13260b81e8
Jon <jbrundagejr@gmail.com>
2021-05-31 20:46:58 -0400

    login working

--- vinyl-app/src/components/Login.js
+++ vinyl-app/src/components/Login.js
@@ -1,7 +1,47 @@
+import {useState} from 'react'
+import {Input, Button} from 'semantic-ui-react'
+import {useHistory} from 'react-router-dom'
+
 function Login(){
+  const [userName, setUserName] = useState("")
+  const [userPassword, setUserPassword] = useState("")
+  const history = useHistory()
+
+  function whatUserNamed(e){
+    setUserName(e.target.value)
+  }
+
+  function whatUserPassworded(e){
+    setUserPassword(e.target.value)
+  }
+
+  function handleClick(e){
+    e.preventDefault()
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name: whatUserNamed, password: whatUserPassworded})
+    })
+     .then(res => res.json())
+     .then(userInfo =>
+      localStorage.token = userInfo.token)
+      history.push('/vinyls')
+  }
+
+  function handleLogout(){
+    localStorage.clear()
+  }
+
   return (
     <div>
-      <h1>Login</h1>
+      <form onSubmit={handleClick}>
+        <Input className="input" label='Name' id="name" value={userName} onChange={whatUserNamed} type="text" placeholder="Your name"></Input>
+        <Input className="input" label='Password' id="userpassword" value={userPassword} onChange={whatUserPassworded} type="password" placeholder="Your password"></Input>
+        <Button>Login</Button>
+      </form>
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
   )
 }
